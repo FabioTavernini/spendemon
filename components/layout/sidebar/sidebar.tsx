93572757d97da9ab3ef39fd9459773ecaae90774
FabioTavernini <FabioTavernini@proton.me>
2026-04-02 11:29:08 +0200

    sidebar title

--- components/layout/sidebar/sidebar.tsx
+++ components/layout/sidebar/sidebar.tsx
@@ -16,43 +16,42 @@ import {
     DropdownMenuItem
 } from "@/components/ui/dropdown-menu"
 
-
 import { ModeToggle } from "@/components/themeselector"
-
 import { ChevronDown } from "lucide-react"
 import { Separator } from "@/components/ui/separator"
-
 
 export function AppSidebar() {
     return (
         <Sidebar>
-            <SidebarHeader>
-                <SidebarMenu>
+            <SidebarHeader className="flex flex-col gap-4">
+                <h2 className="text-lg font-semibold truncate">My App</h2>
 
+                <SidebarMenu>
                     <SidebarMenuItem>
                         <ModeToggle />
                     </SidebarMenuItem>
-
-
-                    <Separator className="my-5" />
-
-                    <SidebarMenuItem>
-                        <DropdownMenu>
-                            <DropdownMenuTrigger asChild>
-                                <SidebarMenuButton>
-                                    Select Cluster
-                                    <ChevronDown className="ml-auto" />
-                                </SidebarMenuButton>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
-                                <DropdownMenuItem>
-                                    <span>Acme Inc</span>
-                                </DropdownMenuItem>
-                            </DropdownMenuContent>
-                        </DropdownMenu>
-                    </SidebarMenuItem>
                 </SidebarMenu>
             </SidebarHeader>
+
+            <Separator className="my-4" />
+
+            <SidebarMenu className="px-2">
+                <SidebarMenuItem>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <SidebarMenuButton className="justify-between">
+                                Select Cluster
+                                <ChevronDown className="ml-2" />
+                            </SidebarMenuButton>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuItem>
+                                <span>Acme Inc</span>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </SidebarMenuItem>
+            </SidebarMenu>
         </Sidebar>
     )
 }
