a0fb2b8613f7d081fd1efacdcbf54d5a87f58a59
FabioTavernini <FabioTavernini@proton.me>
2026-04-02 10:54:06 +0200

    transfer to ts

--- backend/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/FabioTavernini/spendemon/backend
-
-go 1.26.1
-
-require gopkg.in/yaml.v3 v3.0.1
--- backend/internal/metrics/provider.go
+++
@@ -1,7 +0,0 @@
-package metrics
-
-import "context"
-
-type Provider interface {
-	NamespaceValues(ctx context.Context) ([]string, error)
-}
