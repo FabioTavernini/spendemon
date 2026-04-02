faa6affffd02f2ba81d2158c2a37a79bb4f2b065
FabioTavernini <FabioTavernini@proton.me>
2026-04-02 11:22:45 +0200

    sidebar content

--- components/layout/sidebar/sidebar.tsx
+++ components/layout/sidebar/sidebar.tsx
@@ -20,30 +20,39 @@ import {
 import { ModeToggle } from "@/components/themeselector"
 
 import { ChevronDown } from "lucide-react"
+import { Separator } from "@/components/ui/separator"
 
 
 export function AppSidebar() {
     return (
-<Sidebar>
-  <SidebarHeader>
-    <SidebarMenu>
-      <SidebarMenuItem>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <SidebarMenuButton>
-              Select Workspace
-              <ChevronDown className="ml-auto" />
-            </SidebarMenuButton>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
-            <DropdownMenuItem>
-              <span>Acme Inc</span>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </SidebarMenuItem>
-    </SidebarMenu>
-  </SidebarHeader>
-</Sidebar>
+        <Sidebar>
+            <SidebarHeader>
+                <SidebarMenu>
+
+                    <SidebarMenuItem>
+                        <ModeToggle />
+                    </SidebarMenuItem>
+
+
+                    <Separator className="my-5" />
+
+                    <SidebarMenuItem>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <SidebarMenuButton>
+                                    Select Cluster
+                                    <ChevronDown className="ml-auto" />
+                                </SidebarMenuButton>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
+                                <DropdownMenuItem>
+                                    <span>Acme Inc</span>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+        </Sidebar>
     )
 }
--- components/themeselector.tsx
+++ components/themeselector.tsx
@@ -1,6 +1,6 @@
 "use client"
 import * as React from "react"
-import { Moon, Sun } from "lucide-react"
+import { ChevronDown, Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
 import {
@@ -9,6 +9,7 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { SidebarMenuButton } from "./ui/sidebar"
 
 
 export function ModeToggle() {
@@ -17,12 +18,12 @@ export function ModeToggle() {
     const { setTheme } = useTheme()
     return (
         <DropdownMenu>
+
             <DropdownMenuTrigger asChild>
-                <Button variant="ghost" size="icon">
-                    <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-                    <span className="sr-only">{"Theme"}</span> {/* screenreader label */}
-                </Button>
+                <SidebarMenuButton>
+                    Select Theme
+                    <ChevronDown className="ml-auto" />
+                </SidebarMenuButton>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
                 <DropdownMenuItem onClick={() => setTheme("light")}>
