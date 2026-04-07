1efb4a0cb45a1d68883a493b1ae4324fc53523bd
FabioTavernini <FabioTavernini@proton.me>
2026-04-07 18:57:05 +0200

    set url

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -17,10 +17,8 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  url: 'https://spendemon.netlify.app', // Url to your site with no trailing slash
+  baseUrl: '/', // Base directory of your site relative to your repo
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
